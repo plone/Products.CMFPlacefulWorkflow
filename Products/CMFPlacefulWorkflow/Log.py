bc86d8aecde120700e571ce166e518169b233553
Encolpe Degoute <encolpe@gmail.com>
2006-03-21 11:30:15 +0000

    add missing licence headers
    svn path=/CMFPlacefulWorkflow/branches/plone2.5/; revision=21119

--- Products/CMFPlacefulWorkflow/Log.py
+++ Products/CMFPlacefulWorkflow/Log.py
@@ -1,6 +1,20 @@
-# ****************************
-# ****** LOG FACILITIES ******
-# ****************************
+# -*- coding: utf-8 -*-
+## CMFPlacefulWorkflow
+## Copyright (C)2006 Ingeniweb
+
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
 One can override the following variables :
 
@@ -21,6 +35,11 @@ LOG_PROCESSOR : A dictionnary holding, for each key, the data processor.
 A data processor is a function that takes only one parameter : the data to print.
 Default : LogFile for all keys.
 """
+__version__ = "$Revision$"
+# $Source: /cvsroot/ingeniweb/PloneSubscription/SubscriptionTool.py,v $
+# $Id$
+__docformat__ = 'restructuredtext'
+
 
 
 LOG_LEVEL = -1
