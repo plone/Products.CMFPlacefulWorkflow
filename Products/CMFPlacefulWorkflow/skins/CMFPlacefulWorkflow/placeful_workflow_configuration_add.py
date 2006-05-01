7808a7459b063b4792435737fea00a584bbc9daa
Encolpe Degoute <encolpe@gmail.com>
2006-05-01 18:10:25 +0000

    better explaination for this configuration panel
    svn path=/CMFPlacefulWorkflow/branches/plone2.5/; revision=22925

--- Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/placeful_workflow_configuration_add.py
+++ Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/placeful_workflow_configuration_add.py
@@ -6,5 +6,5 @@ from Products.CMFPlacefulWorkflow import CMFPlacefulWorkflowMessageFactory as _
 
 context.manage_addProduct['CMFPlacefulWorkflow'].manage_addWorkflowPolicyConfig()
 
-context.plone_utils.addPortalMessage(_(u'Workflow policy configuration added'))
+context.plone_utils.addPortalMessage(_(u'Workflow policy configuration added.'))
 context.REQUEST.RESPONSE.redirect('placeful_workflow_configuration')
