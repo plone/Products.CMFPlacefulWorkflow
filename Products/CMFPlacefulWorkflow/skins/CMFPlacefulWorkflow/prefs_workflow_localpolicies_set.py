7410b94497b5ae77cc837c01e61b43f9d6b696a6
Encolpe Degoute <encolpe@gmail.com>
2006-06-02 16:28:35 +0000

    polishing
    svn path=/CMFPlacefulWorkflow/branches/plone2.5/; revision=24290

--- Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_localpolicies_set.py
+++ Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_localpolicies_set.py
@@ -3,21 +3,24 @@
 ##
 
 from Products.CMFPlacefulWorkflow import CMFPlacefulWorkflowMessageFactory as _
+from Products.CMFCore.utils import getToolByName
 
 request = context.REQUEST
 
 policy_ids = request.get('policy_ids',[])
 policy_id = request.get('policy_id', None)
+policy_duplicate_id = request.get('policy_duplicate_id', 'empty')
 
-pwt = context.portal_placeful_workflow
+pw_tool = getToolByName(context, 'portal_placeful_workflow')
 
 if delete and policy_ids:
     for policy_id in policy_ids:
-        if policy_id in pwt.objectIds():
-            pwt.manage_delObjects([policy_id,])
+        if policy_id in pw_tool.objectIds():
+            pw_tool.manage_delObjects([policy_id,])
     context.plone_utils.addPortalMessage(_(u'Deleted Local Workflow Policy.'))
     context.REQUEST.RESPONSE.redirect('prefs_workflow_localpolicies_form')
+
 elif add and policy_id:
-    pwt.manage_addWorkflowPolicy(id = policy_id)
+    pw_tool.manage_addWorkflowPolicy(id=policy_id, duplicate_id=policy_duplicate_id)
     context.plone_utils.addPortalMessage(_(u'Local Workflow Policy added.'))
     context.REQUEST.RESPONSE.redirect('prefs_workflow_policy_mapping?wfpid='+policy_id)
