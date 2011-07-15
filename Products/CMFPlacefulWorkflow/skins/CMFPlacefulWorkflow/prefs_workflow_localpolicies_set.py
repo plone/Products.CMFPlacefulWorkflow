fccf0c4e4c7ca5f4d39d46b5d09ce839e1ac5b59
Encolpe Degoute <encolpe@gmail.com>
2011-07-15 15:31:45 +0000

    Fix #9833: an empty required fields will display an error in the portal status message
    svn path=/Products.CMFPlacefulWorkflow/trunk/; revision=242476

--- Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_localpolicies_set.py
+++ Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_localpolicies_set.py
@@ -12,15 +12,21 @@ policy_id = request.get('policy_id', None)
 policy_duplicate_id = request.get('policy_duplicate_id', 'empty')
 
 pwtool = getToolByName(context, 'portal_placeful_workflow')
+plone_utils = getToolByName(context, 'plone_utils')
 
 if delete and policy_ids:
     for policy_id in policy_ids:
         if policy_id in pwtool.objectIds():
             pwtool.manage_delObjects([policy_id,])
-    context.plone_utils.addPortalMessage(_(u'Deleted Local Workflow Policy.'))
-    context.REQUEST.RESPONSE.redirect('prefs_workflow_localpolicies_form')
+    plone_utils.addPortalMessage(_(u'Deleted Local Workflow Policy.'), 'info')
+    request.RESPONSE.redirect('prefs_workflow_localpolicies_form')
 
-elif add and policy_id:
-    pwtool.manage_addWorkflowPolicy(id=policy_id, duplicate_id=policy_duplicate_id)
-    context.plone_utils.addPortalMessage(_(u'Local Workflow Policy added.'))
-    context.REQUEST.RESPONSE.redirect('prefs_workflow_policy_mapping?wfpid='+policy_id)
+elif add:
+    if policy_id:
+        pwtool.manage_addWorkflowPolicy(id=policy_id, duplicate_id=policy_duplicate_id)
+        plone_utils.addPortalMessage(_(u'Local Workflow Policy added.'), 'info')
+        request.RESPONSE.redirect('prefs_workflow_policy_mapping?wfpid='+policy_id)
+
+    else:
+        plone_utils.addPortalMessage(_(u'The policy Id is required.'), 'error')
+        request.RESPONSE.redirect('prefs_workflow_localpolicies_form')
--- Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_policy_mapping_set.py
+++ Products/CMFPlacefulWorkflow/skins/CMFPlacefulWorkflow/prefs_workflow_policy_mapping_set.py
@@ -5,10 +5,20 @@
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlacefulWorkflow import CMFPlacefulWorkflowMessageFactory as _
 
+plone_utils = getToolByName(context, 'plone_utils')
+
 request = context.REQUEST
 policy = getToolByName(context, 'portal_placeful_workflow').getWorkflowPolicyById(wfpid)
 
-policy.setTitle(title)
+if title:
+    plone_utils.addPortalMessage(title)
+    policy.setTitle(title)
+else:
+    plone_utils.addPortalMessage(_(u'Title is required.'), 'error')
+    if request:
+        request.RESPONSE.redirect('prefs_workflow_policy_mapping?wfpid=%s' % wfpid)
+    return request
+
 policy.setDescription(description)
 
 policy.setDefaultChain(default_chain=(default_workflow_id,),REQUEST=context.REQUEST)
@@ -25,7 +35,7 @@ for pt, wf in wf.items():
 wf_tool = getToolByName(context, 'portal_workflow')
 wf_tool.updateRoleMappings()
 
-context.plone_utils.addPortalMessage(_(u'Changes to criteria saved.'))
+plone_utils.addPortalMessage(_(u'Changes to criteria saved.'))
 if request:
     request.RESPONSE.redirect('prefs_workflow_policy_mapping?wfpid=%s' % wfpid)
 
