c9d386509067bf00dc355285b8c3999207c47fda
alec <alec@alec.edw.ro>
2012-09-10 14:57:16 +0300

    Fixed new 'CMFPlacefulWorkflow: Manage workflow policies' permission assignment
    Fixed new 'CMFPlacefulWorkflow: Manage workflow policies' permission assignment
    by calling setDefaultRoles. Added it under package profile.

--- Products/CMFPlacefulWorkflow/__init__.py
+++ Products/CMFPlacefulWorkflow/__init__.py
@@ -23,8 +23,6 @@ __version__ = "$Revision: 62604 $"
 # $Id: __init__.py 62604 2008-04-13 20:50:14Z hannosch $
 __docformat__ = 'restructuredtext'
 
-ManageWorkflowPolicies = 'Manage workflow policies'
-
 import PlacefulWorkflowTool
 import DefaultWorkflowPolicy
 import WorkflowPolicyConfig
@@ -54,7 +52,7 @@ def initialize(context):
 
     context.registerClass(
         WorkflowPolicyConfig.WorkflowPolicyConfig,
-        permission='Add Workflow Policy',
+        permission='Add Workflow Policy XXX',
         constructors=(WorkflowPolicyConfig.manage_addWorkflowPolicyConfigForm,
                       WorkflowPolicyConfig.manage_addWorkflowPolicyConfig),
         icon='www/WorkflowPolicyConfig_icon.gif',
