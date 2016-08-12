8d246124e7a5064a5205ea06aaebcd1cb5337d8d
Maurits van Rees <maurits@vanrees.org>
2016-08-12 22:29:58 +0200

    Added Plone 5.1 classifier to setup.py.
        [ci skip]

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='Products.CMFPlacefulWorkflow',
       classifiers=[
           "Framework :: Plone",
           "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.1",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2.7",
           ],
